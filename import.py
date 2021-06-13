3dc2e93d084833a02b92687d6204b5e1f271548f
mconnellyada <44677392+mconnellyada@users.noreply.github.com>
2021-06-13 19:00:04 +0100

    Create import.py

---
+++ import.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv(
+    filepath_or_buffer='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
+    header=None,
+    sep=',')
+
+df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
+df.dropna(how="all", inplace=True) # drops the empty line at file-end
+
+df.tail()
